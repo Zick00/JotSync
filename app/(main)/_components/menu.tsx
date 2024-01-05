e2552b73a25eed266d681db85ede247960716aa8
dariosorrentino <74456158+dariosorrentino@users.noreply.github.com>
2024-01-05 11:15:55 +0100

    Add files via upload

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -1,74 +1,75 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { Button } from "@/components/ui/button";
-import { MoreHorizontal, Trash } from "lucide-react";
-import { api } from "@/convex/_generated/api";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/clerk-react";
-import { Id } from "@/convex/_generated/dataModel";
 import { useMutation } from "convex/react";
 import { toast } from "sonner";
-import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuSeparator
-  } from "@/components/ui/dropdown-menu";
+import { MoreHorizontal, Trash } from "lucide-react";
 
-  interface MenuProps {
-    documentId: Id<"documents">;
+import { Id } from "@/convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface MenuProps {
+  documentId: Id<"documents">;
+};
+
+export const Menu = ({
+  documentId
+}: MenuProps) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const archive = useMutation(api.documents.archive);
+
+  const onArchive = () => {
+    const promise = archive({ id: documentId })
+
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash!",
+      error: "Failed to archive note."
+    });
+
+    router.push("/documents");
   };
-  
-  export const Menu = ({
-    documentId
-  }: MenuProps) => {
-    const router = useRouter();
-    const { user } = useUser();
-  
-    const archive = useMutation(api.documents.archive);
-  
-    const onArchive = () => {
-      const promise = archive({ id: documentId })
-  
-      toast.promise(promise, {
-        loading: "Moving to trash...",
-        success: "Note moved to trash!",
-        error: "Failed to archive note."
-      });
-  
-      router.push("/documents");
-    };
-  
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button size="sm" variant="ghost">
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent 
-          className="w-60" 
-          align="end" 
-          alignOffset={8} 
-          forceMount
-        >
-          <DropdownMenuItem onClick={onArchive}>
-            <Trash className="h-4 w-4 mr-2" />
-            Delete
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <div className="text-xs text-muted-foreground p-2">
-            Last edited by: {user?.fullName}
-          </div>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  };
-  
-  Menu.Skeleton = function MenuSkeleton() {
-    return (
-      <Skeleton className="h-10 w-10" />
-    )
-  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent 
+        className="w-60" 
+        align="end" 
+        alignOffset={8} 
+        forceMount
+      >
+        <DropdownMenuItem onClick={onArchive}>
+          <Trash className="h-4 w-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="text-xs text-muted-foreground p-2">
+          Last edited by: {user?.fullName}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+Menu.Skeleton = function MenuSkeleton() {
+  return (
+    <Skeleton className="h-10 w-10" />
+  )
+}
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,47 +1,46 @@
 "use client";
 
-import { useConvexAuth} from "convex/react";
-import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
-import { Spinner } from "@/components/spinner";
-
 import { SignInButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
-export const Heading = () => {
-const { isAuthenticated, isLoading } = useConvexAuth();
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
-    return(
-        <div className= "max-w-3xl space-y-4">
-        <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-            i tuoi appunti, la tua logica, tutto unificato. benvenuti su 
-            <span className= "underline"> JotSync</span>
-        </h1>
-        <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-            JotSync è un ambiente di lavoro <br />
-            rapido,semplice, intuitivo.
-        </h3>
-        {isLoading &&(
-            <div className="w-full flex item-center justify-center">
-            <Spinner size="lg"/>
-            </div>
-        )}
-        {isAuthenticated && !isLoading &&(
-        <Button asChild>
-        <Link href="/documents">
-        Entra su JotSync
-        <ArrowRight className="h-4 w-4 ml-2"/>
-        </Link>
-        </Button>
-        )}
-        {!isAuthenticated && !isLoading && (
-            <SignInButton mode="modal">
-            <Button>
-             Get JotSync Free
-             <ArrowRight className="h-4 w-4 ml-2"/>
-            </Button>
-            </SignInButton>
-        )}
+export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">JotSync</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        JotSync is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
         </div>
-    )
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter JotSynch
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get JotSynch free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+    </div>
+  )
 }
